459d77b9befe4d037bc45645554bc1d66df2cc17
Mike Terry <support@miketerry.org>
2026-02-20 07:46:13 -0600

    first commit of new package

---
+++ index.js
@@ -0,0 +1,7 @@
+// index..js:
+
+import buildObjectProcessor from "./lib/buildObjectProcessor.js";
+import processorRegistry from "./lib/processorRegistry.js";
+
+export { buildObjectProcessor };
+export default processorRegistry;
